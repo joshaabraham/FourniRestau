a757a0361406375e013e35da62891f16a08c3eda
remi <duplanremi@gmail.com>
2022-09-14 19:35:21 +0200

    creation Producteur controlleur

---
+++ PanierManagementApi/Controllers/PanierController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PanierManagement;
+using PanierManagementApi.Contexts;
+
+namespace PanierManagementApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PanierController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public PanierController(ApplicationDBContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Panier>>> Paniers()
+        {
+
+            try
+            {
+                return Ok(await _context.Paniers.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Panier>> Get(Guid id)
+        {
+
+            try
+            {
+                var panier = _context.Paniers.Find(id);
+                if (panier == null)
+                    return NotFound("No user was found.");
+                return Ok(await _context.Paniers.SingleAsync());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Panier>>> PostUser(Panier panier)
+        {
+
+            _context.Paniers.Add(panier);
+            _context.SaveChangesAsync();
+
+            return Ok(await _context.Paniers.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Panier>> UpdateUser(Panier panier)
+        {
+            try
+            {
+                var panierToUpdate = _context.Paniers.FindAsync(panier.ID);
+                if (panierToUpdate == null)
+                    return NotFound("No user was found.");
+
+                _context.Paniers.Remove(panierToUpdate.Result);
+                _context.Paniers.Add(panier);
+                return Ok(await _context.Paniers.SingleAsync(x => x.ID == panier.ID));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Panier>> DeleteUser(Panier panier)
+        {
+            try
+            {
+                var panierToDelete = _context.Paniers.Find(panier.ID);
+                if (panierToDelete == null)
+                    return NotFound("No user was found.");
+
+                _context.Paniers.Remove(panierToDelete);
+
+
+                return Ok(await _context.Paniers.ToListAsync());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ProducteurManagementApi/Controllers/ProducteurController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProducteurManagement;
+using ProducteurManagementApi.Contexts;
+
+namespace ProducteurManagementApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProducteurController : ControllerBase
+    {
+
+        private readonly ApplicationDBContext _context;
+
+        public ProducteurController(ApplicationDBContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Producteur>>> Producteurs()
+        {
+
+            try
+            {
+                return Ok(await _context.Producteurs.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Producteur>> Get(Guid id)
+        {
+
+            try
+            {
+                var producteur = _context.Producteurs.Find(id);
+                if (producteur == null)
+                    return NotFound("No user was found.");
+                return Ok(await _context.Producteurs.SingleAsync());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Producteur>>> PostUser(Producteur producteur)
+        {
+
+            _context.Producteurs.Add(producteur);
+            _context.SaveChangesAsync();
+
+            return Ok(await _context.Producteurs.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Producteur>> UpdateUser(Producteur producteur)
+        {
+            try
+            {
+                var producteurToUpdate = _context.Producteurs.FindAsync(producteur.ID);
+                if (producteurToUpdate == null)
+                    return NotFound("No user was found.");
+
+                _context.Producteurs.Remove(producteurToUpdate.Result);
+                _context.Producteurs.Add(producteur);
+                return Ok(await _context.Producteurs.SingleAsync(x => x.ID == producteur.ID));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Producteur>> DeleteUser(Producteur producteur)
+        {
+            try
+            {
+                var producteurToDelete = _context.Producteurs.Find(producteur.ID);
+                if (producteurToDelete == null)
+                    return NotFound("No user was found.");
+
+                _context.Producteurs.Remove(producteurToDelete);
+
+
+                return Ok(await _context.Producteurs.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+    }
+}
